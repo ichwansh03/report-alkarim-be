b2b9b542d4b695fe04f4bf11ac19df063e3fc12f
ichwansh03 <ichwansholihin70@gmail.com>
2025-11-10 20:54:16 +0700

    refactor refresh token service

--- src/main/java/org/ichwan/util/RefreshTokenService.java
+++ src/main/java/org/ichwan/util/RefreshTokenService.java
@@ -26,22 +26,22 @@ public class RefreshTokenService {
 
     public RefreshToken createRefreshToken(Long userId) {
         RefreshToken refreshToken = new RefreshToken();
-        refreshToken.setToken(UUID.randomUUID() + "-" + userId);
-        refreshToken.setUserId(userService.finById(userId).getId());
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setUserId(userId);
         refreshToken.setExpireAt(Instant.now().plus(7, ChronoUnit.DAYS));
         return refreshToken;
     }
 
     public Optional<RefreshToken> validateRefreshToken(String token) {
         RefreshToken byToken = repository.findByToken(token);
-        if (byToken == null || byToken.isExpired()) {
+        if (byToken == null || byToken.getExpireAt().isBefore(Instant.now())) {
             return Optional.empty();
         }
         return Optional.of(byToken);
     }
 
     public RefreshToken changeToken(RefreshToken prevToken) {
-        repository.delete(prevToken.getUserId().toString());
+        repository.delete("userId", prevToken.getUserId());
         return createRefreshToken(prevToken.getUserId());
     }
 }
