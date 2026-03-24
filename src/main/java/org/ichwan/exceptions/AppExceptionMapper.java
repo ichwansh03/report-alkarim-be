bce0d3f88f68812a1438bf583b5ba4e5b3af261c
ichwansh03 <ichwansholihin70@gmail.com>
2026-03-24 18:53:23 +0700

    add provider annotation

--- src/main/java/org/ichwan/exceptions/AppExceptionMapper.java
+++ src/main/java/org/ichwan/exceptions/AppExceptionMapper.java
@@ -5,8 +5,10 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.UriInfo;
 import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 import org.ichwan.dto.ErrorResponse;
 
+@Provider
 public class AppExceptionMapper implements ExceptionMapper<AppException> {
 
     @Context
